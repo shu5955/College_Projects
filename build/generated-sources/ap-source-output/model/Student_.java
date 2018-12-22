d2f84e4dc8c6f93d24885a94f1b6d3516d91ee95
shu5955 <shub.sharma350@gmail.com>
2018-12-21 22:42:09 -0600

    Simple web application written in Java

---
+++ build/generated-sources/ap-source-output/model/Student_.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.2.0.v20110202-r8913", date="2011-10-08T08:44:39")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, String> address;
+    public static volatile SingularAttribute<Student, String> studentID;
+    public static volatile SingularAttribute<Student, String> name;
+
+}
